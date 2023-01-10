0a24e708586f9383301ad7f33ad405b3f1dbc4c1
phil13131 <pd131.2574@gmail.com>
2023-01-10 18:33:06 +0000

    script file updated with ammended password length

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generatePassword() {
 
   // get input and validate
   numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
-  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+  if (numberOfCharacters < 10 || numberOfCharacters > 64) {
     return "Please choose a valid number of characters.";
   } else if (isNaN(numberOfCharacters)) {
     numberOfCharacters = prompt("Please enter a valid number.");
